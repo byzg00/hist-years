d6265b4cfdc9a9d0b029f7eff751d9880c9596fa
byzg <byzg00@gmail.com>
2025-09-23 00:24:49 +0300

    remove compiled js

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import AppRouter from './router';
-import GlobalStyles from './GlobalStyles';
-var App = function () {
-    return (_jsxs(_Fragment, { children: [_jsx(GlobalStyles, {}), _jsx(AppRouter, {})] }));
-};
-export default App;
--- src/components/periods/Title.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { TitleLine, TitleText, TitleWrapper } from './styled';
-export var Title = function (props) {
-    var title = props.title;
-    return (_jsxs(TitleWrapper, { children: [_jsx(TitleLine, {}), _jsx(TitleText, { children: title })] }));
-};
